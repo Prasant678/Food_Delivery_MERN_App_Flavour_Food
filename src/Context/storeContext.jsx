71e70519ac1cd2d96fbb61f381a9075fc3727987
G Prasant <prasangtrao917@gmail.com>
2025-10-21 17:26:44 +0530

    Update storeContext.jsx

--- src/Context/storeContext.jsx
+++ src/Context/storeContext.jsx
@@ -7,7 +7,7 @@ const StoreContextProvider = (props) => {
 
     const [cartItems, setCartItems] = useState({});
 
-    const url = "http://localhost:5000"
+    const url = "https://food-delivery-mern-app-flavour-food.onrender.com"
 
     const [token, setToken] = useState("");
 
